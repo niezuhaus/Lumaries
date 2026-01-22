3bb285c63e3d4328ebe342e2442d0774244bab92
dani* <53942131+niezuhaus@users.noreply.github.com>
2026-01-22 22:04:32 +0100

    first

---
+++ prisms.ino
@@ -0,0 +1,89 @@
+#include <Stepper.h>
+
+#define SENSOR_PIN 12
+#define LDR_PIN A1
+
+#define STEP_PIN 54
+#define DIR_PIN 55
+#define ENABLE_PIN 38
+
+const int stepsPerRevolution = 2048;  // 28BYJ-48
+const int lightThreshold = 800;
+const int minSpeed = 7;
+const int maxSpeed = 15;
+const int potiMin = 480;
+const int potiMax = 520;
+const int bounceWaitSteps = 50;
+
+int adcPin = A0;
+int lastPotiIn = 0;
+int potiIn = 0;
+int vel = 10;
+int steps = 0;
+bool reverse = false;
+int magnet;
+int light = 0;
+int lightMeasures[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int index = 0;
+int deafSteps = 0;
+
+Stepper myStepper(
+  stepsPerRevolution,
+  8, 10, 9, 11  // Reihenfolge wichtig!
+);
+
+void setup() {
+  Serial.begin(9600);                 // init serial to 9600b/s
+  pinMode(LED_BUILTIN, OUTPUT);       // set ledPin to OUTPUT
+  pinMode(SENSOR_PIN, INPUT_PULLUP);  // interner Pull-up
+  myStepper.setSpeed(25);             // RPM
+}
+
+void loop() {
+  if (deafSteps > 0) {
+    deafSteps--;
+    magnet = HIGH;
+    light = 500;
+  } else if (potiIn > 520 || potiIn < 480) {
+    magnet = digitalRead(SENSOR_PIN);
+    // light = analogRead(LDR_PIN);
+    lightMeasures[index] = light;
+    index++;
+    index %= 10;
+
+    Serial.print(average());
+    Serial.print(" ");
+    Serial.println(light);
+  }
+
+  potiIn = analogRead(adcPin);
+
+  if (magnet == LOW || average() > lightThreshold) {
+    digitalWrite(LED_BUILTIN, HIGH);  // if adcIn > 500, led light
+    reverse = !reverse;
+    deafSteps = bounceWaitSteps;
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  if (potiIn > potiMax) {
+    vel = map(potiIn, 512, 1024, minSpeed, maxSpeed);
+    steps = reverse ? -10 : 10;
+  } else if (potiIn < 504) {
+    vel = map(potiIn, 0, 512, maxSpeed, minSpeed);
+    steps = reverse ? 10 : -10;
+  }
+  myStepper.setSpeed(vel);
+  if ((potiIn > potiMax || potiIn < potiMin)) {
+    myStepper.step(steps);
+    lastPotiIn = potiIn;
+  }
+}
+
+int average() {
+  int sum = 0;
+  for (int i = 0; i < 10; i++) {
+    sum += lightMeasures[i];
+  }
+  return sum / 10;
+}
