2022e54d745d159b2422b7b7b1d0059f5c0ac3c2
dani* <53942131+niezuhaus@users.noreply.github.com>
2026-06-06 13:56:47 +0200

    thumb stick test

--- thumb_stick/thumb_stick.ino
+++ thumb_stick/thumb_stick.ino
@@ -1,23 +1,32 @@
-const int PIN_X   = A0;
-const int PIN_Y   = A1;
-const int PIN_BTN = 2;
+const int PIN_X = A11;
+const int PIN_Y = A12;
+const int PIN_Z = 21;
+
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PIN_BTN, INPUT_PULLUP);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
 void loop() {
-  int x       = analogRead(PIN_X);
-  int y       = analogRead(PIN_Y);
-  bool clicked = digitalRead(PIN_BTN) == LOW;
+  int x = analogRead(PIN_X);
+  int y = analogRead(PIN_Y);
+  int z = digitalRead(PIN_Z);
+
 
   // Serial.print("X: ");
   Serial.print(x);
   Serial.print(" ");
   Serial.print(y);
-  Serial.print("  BTN: ");
-  Serial.println(clicked ? "PRESSED" : "released");
+  Serial.print(" ");
+  Serial.println(z);
+
+  int x1 = map(x, 0, 1023, 0, 255);
+  int y1 = map(y, 0, 1023, 0, 255);
+
+  analogWrite(5, x1);
+  analogWrite(6, y1);
 
   delay(50);
 }
